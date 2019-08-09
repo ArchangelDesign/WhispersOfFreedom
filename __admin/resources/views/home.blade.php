97a4f1d40255f7531ec1d770ea74c0c6cafb15a1
Raff Martinez <archangel.raffael@gmail.com>
2019-08-09 18:02:05 -0400

    login form reworked

--- __admin/resources/views/home.blade.php
+++ __admin/resources/views/home.blade.php
@@ -1,7 +1,67 @@
-
 @include('header-normal')
 
+<div class="container">
+
+    <div class="row">
+
+        <div class="col-lg-4">
+
+            <div class="thumbnail">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/Bz2oXcCudNk" frameborder="0"
+                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                            allowfullscreen></iframe>
+                </div>
+                <div class="caption">
+                    <h4>Wof #9</h4>
+                    <p>
+                        Some cleanup was required, heartbeat was coming even after the client has disconnected. Now
+                        along with heartbeat we're sending some stats about the load of the server. That will be crucial
+                        for live testing, hopefully next month I'll be uploading the binary to the server but first we
+                        need CI to be set up.
+                    </p>
+                </div>
+
+            </div>
+        </div>
 
 
+        <div class="col-lg-4">
+            <div class="thumbnail">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/lAWZ3nFe5Io" frameborder="0"
+                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                            allowfullscreen></iframe>
+                </div>
+
+                <div class="caption">
+                    <h4>Wof #8</h4>
+                    <p>
+                        First major functionality is starting to come together. Some new problems have occurred but
+                        nothing seriously holding me back. If everything goes well, in the next video I will finish
+                        Lobby on the client side, listing connected clients and allowing to exit lobby and start combat.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-4">
+            <div class="thumbnail">
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/SesLqs4Jo9A" frameborder="0"
+                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                            allowfullscreen></iframe>
+                </div>
+
+                <div class="caption">
+                    <h4>Wof #7</h4>
+                    <p>
+                        Working on battle list in the lobby and implementing fixes to TCP/IP server.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+    </div>
 
 @include('footer-normal')
--- __admin/resources/views/login.blade.php
+++ __admin/resources/views/login.blade.php
@@ -1,8 +1,34 @@
-<h4>Welcome</h4>
+@include('header-normal')
 
-<form method="post" action="{{ route('do-login') }}">
-@csrf
-    <input name="username" type="text">
-    <input name="password" type="password">
-    <input type="submit" value="Login">
-</form>
+<div class="container">
+    <div class="row">
+        <div class="col-xl-4 col-md-6 col-lg-6 col-sm-8 col-xs-14 col-xl-offset-4 col-lg-offset-3 col-md-offset-3 col-sm-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h4>Login</h4>
+                </div>
+                <div class="panel-body">
+
+                    <form method="post" action="{{ route('do-login') }}">
+                        @csrf
+                        <div class="form-group">
+                            <label for="username">Username or email</label>
+                            <input class="form-control" id="username" name="username" type="text">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input class="form-control" id="password" name="password" type="password">
+                        </div>
+
+                        <div class="form-group">
+                            <input class="btn btn-primary pull-right" type="submit" value="Login">
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@include('footer-normal')

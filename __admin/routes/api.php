343778d1da3ab69829951710fc0d08e29f17e861
Raff Martinez <archangel.raffael@gmail.com>
2019-08-18 12:08:15 -0400

    credentials verification api endpoint

--- __admin/routes/api.php
+++ __admin/routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/auth/verify', 'UserController@verifyCredentials')->name('verify_credentials');
+Route::post('/auth/verify', 'UserController@verifyCredentials')->name('verify_credentials');

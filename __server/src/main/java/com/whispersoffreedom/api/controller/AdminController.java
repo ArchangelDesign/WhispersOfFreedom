23f39613397d141595e3e3374ef82ac76f62a8a2
Raff Martinez <archangel.raffael@gmail.com>
2019-10-20 12:51:12 -0400

    Admin controller: get list of connected users

---
+++ __server/src/main/java/com/whispersoffreedom/api/controller/AdminController.java
@@ -0,0 +1,25 @@
+package com.whispersoffreedom.api.controller;
+
+import com.whispersoffreedom.server.Client;
+import com.whispersoffreedom.server.WofServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping("/get-clients")
+    public List<Client> getClientList(
+            @RequestHeader(name = "session-token") String sessionToken
+    ) {
+        if (WofServer.getClientCount() == 0)
+            return null;
+
+        return WofServer.getAllClients();
+    }
+}
